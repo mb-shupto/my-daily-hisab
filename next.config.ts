bd1077a62e091fa76712edfcf9e2f84c7afe6278
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-08-20 13:14:47 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'icons.veryicon.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
